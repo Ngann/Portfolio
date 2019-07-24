de9dc505f3b41436b2e2e72d0c2a2ee365e61242
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-07-24 16:42:03 -0700

    render list of jobs on home.html

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
 def home(request):
-    return render(request, 'jobs/home.html')
+    # variable jobs is equal to job objects from the database, class.objects
+    # {'jobs': jobs} dictionary to render the objects
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs': jobs})
