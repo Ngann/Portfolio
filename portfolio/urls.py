7ddde6cfa6afe30afdc40b9159bbcb445f256397
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-07-24 17:04:05 -0700

    add static folder

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 import jobs.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('', jobs.views.home, name='home'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
